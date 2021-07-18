5983b99b492df9b6ebc074d6669cc1878234e52e
Jesse Granger <jesse_granger@fastmail.com>
2021-07-18 10:04:58 -0700

    bring ParseEnt into Flask/Manager

--- Plugins/Follower/FlaskManager.cs
+++ Plugins/Follower/FlaskManager.cs
@@ -13,6 +13,15 @@ namespace Follower
     static class FlaskManager
     {
 
+        public static bool IsFlask(Entity ent)
+        {
+            if (!IsValid(ent)) return false;
+            string pathName = ent.Path.Substring(ent.Path.LastIndexOf("/") + 1);
+            return pathName.StartsWith("Flask");
+        }
+
+        public static Flask ParseFlask(Entity ent) => Flask.ParseEnt(ent);
+
         private static Dictionary<string, int> baseHealAmount = new Dictionary<string, int>()
         {
             { "FlaskLife1", 70 },
@@ -43,8 +52,57 @@ namespace Follower
             { "FlaskMana11", 1400 },
             { "FlaskMana12", 1800 },
         };
+        private static Dictionary<string, int> baseDuration = new Dictionary<string, int>()
+        {
+            { "FlaskLife1", 6000 },
+            { "FlaskLife2", 7000 },
+            { "FlaskLife3", 7000 },
+            { "FlaskLife4", 7000 },
+            { "FlaskLife5", 7000 },
+            { "FlaskLife6", 7000 },
+            { "FlaskLife7", 7000 },
+            { "FlaskLife8", 7000 },
+            { "FlaskLife9", 7000 },
+            { "FlaskLife10", 7000 },
+            { "FlaskLife11", 7000 },
+            { "FlaskLife12", 7000 },
+            { "FlaskMana1", 4000 },
+            { "FlaskMana2", 4000 },
+            { "FlaskMana3", 4000 },
+            { "FlaskMana4", 4000 },
+            { "FlaskMana5", 4000 },
+            { "FlaskMana6", 4500 },
+            { "FlaskMana7", 5000 },
+            { "FlaskMana8", 5500 },
+            { "FlaskMana9", 6000 },
+            { "FlaskMana10", 6500 },
+            { "FlaskMana11", 6000 },
+            { "FlaskMana12", 4000 },
+            { "FlaskHybrid1", 5000 }, // Large Hybrid
+            { "FlaskHybrid2", 5000 }, // Large Hybrid
+            { "FlaskHybrid3", 5000 }, // Large Hybrid
+            { "FlaskHybrid4", 5000 }, // Large Hybrid
+            { "FlaskHybrid5", 5000 }, // Large Hybrid
+            { "FlaskHybrid6", 5000 }, // Large Hybrid
+            { "FlaskUtility1", 4000 }, // Diamond Flask
+            { "FlaskUtility2", 4000 }, // Ruby Flask
+            { "FlaskUtility3", 4000 }, // Sapphire Flask
+            { "FlaskUtility4", 4000 }, // Topaz Flask
+            { "FlaskUtility5", 4000 }, // Granite Flask
+            { "FlaskUtility6", 4000 }, // Quicksilver Flask
+            { "FlaskUtility7", 3500 }, // Amethyst Flask
+            { "FlaskUtility8", 4000 }, // Quartz Flask
+            { "FlaskUtility9", 4000 }, // Jade Flask
+            { "FlaskUtility10", 4500 }, // Basalt Flask
+            { "FlaskUtility11", 4000 }, // ???
+            { "FlaskUtility12", 5000 }, // Stibnite Flask
+            { "FlaskUtility13", 4000 }, // Sulphur Flask
+            { "FlaskUtility14", 5000 }, // Silver Flask
+            { "FlaskUtility15", 5000 }, // Bismuth Flask
 
-        private class Flask
+        };
+
+        public class Flask
         {
             public static Stopwatch GlobalTimer = new Stopwatch();
             static Flask() => GlobalTimer.Start();
@@ -56,14 +114,14 @@ namespace Follower
             public int ChargesPerUse;
             public int LifeHealAmount;
             public int ManaHealAmount;
+            public int Duration;
             public bool IsInstant;
             public bool IsInstantOnLowLife;
             public bool IsFull => CurrentCharges >= MaxCharges;
             public bool HasEnoughCharge => CurrentCharges >= ChargesPerUse;
             public bool IsUsable(int cooldown) => HasEnoughCharge && GlobalTimer.ElapsedMilliseconds > 200 && ((!Timer.IsRunning) || Timer.ElapsedMilliseconds > cooldown);
-            public Flask(VirtualKeyCode key)
+            public Flask()
             {
-                Key = key;
             }
             public bool UseFlask(int cooldown)
             {
@@ -76,14 +134,87 @@ namespace Follower
                 }
                 return false;
             }
+
+            public override string ToString() => $"{PathName} [{CurrentCharges}/{MaxCharges}] hp:{LifeHealAmount} mp:{ManaHealAmount} {Duration} ms";
+
+            public static Flask ParseEnt(Entity ent, bool linkToConditions = false)
+            {
+                Flask flask = new Flask();
+                string pathName = ent.Path.Substring(ent.Path.LastIndexOf("/") + 1);
+                flask.PathName = pathName;
+                Quality quality = ent.GetComponent<Quality>();
+                Charges charges = ent.GetComponent<Charges>();
+                if (charges != null)
+                {
+                    flask.ChargesPerUse = charges.ChargesPerUse;
+                    flask.CurrentCharges = charges.NumCharges;
+                    flask.MaxCharges = charges.ChargesMax;
+                }
+                baseHealAmount.TryGetValue(pathName, out flask.LifeHealAmount);
+                baseManaAmount.TryGetValue(pathName, out flask.ManaHealAmount);
+                baseDuration.TryGetValue(pathName, out flask.Duration);
+                Mods flaskMods = ent.GetComponent<Mods>();
+                if (flaskMods.ItemMods != null)
+                {
+                    foreach (var mod in flaskMods.ItemMods)
+                    {
+                        switch (mod.Name)
+                        {
+                            case "FlaskIncreasedDuration":
+                                flask.Duration = (int)((float)flask.Duration * ((100 + mod.Value1) / 100f));
+                                break;
+                            case "FlaskExtraCharges":
+                                flask.MaxCharges += mod.Value1;
+                                break;
+                            case "FlaskInstantRecoveryOnLowLife":
+                                flask.IsInstantOnLowLife = true;
+                                flask.LifeHealAmount = (int)((float)flask.LifeHealAmount * .75f);
+                                break;
+                            case "FlaskFullInstantRecovery":
+                                flask.IsInstant = true;
+                                flask.LifeHealAmount = (int)((float)flask.LifeHealAmount * .34f);
+                                flask.Duration = 0;
+                                break;
+                            case "FlaskPartialInstantRecovery":
+                                flask.IsInstant = true;
+                                flask.LifeHealAmount = (int)((float)flask.LifeHealAmount * .25f);
+                                break;
+                            case "FlaskRemovesBleeding":
+                                if (linkToConditions)
+                                {
+                                    LinkConditionToFlask("bleeding", flask);
+                                    LinkConditionToFlask("corrupted_blood", flask);
+                                }
+                                break;
+                            case "FlaskEffectNotRemovedOnFullMana":
+                                flask.ManaHealAmount = (int)((float)flask.ManaHealAmount * .70f);
+                                flask.Duration = (int)((float)flask.Duration * .70f);
+                                break;
+                            case "FlaskRemovesShock": if (linkToConditions) LinkConditionToFlask("shocked", flask); break;
+                            case "FlaskCurseImmunity": if (linkToConditions) LinkConditionToFlask("cursed", flask); break;
+                            case "FlaskDispellsChill": if (linkToConditions) LinkConditionToFlask("frozen", flask); break;
+                            case "FlaskDispellsBurning": if (linkToConditions) LinkConditionToFlask("burning", flask); break;
+                            case "FlaskDispellsPoison": if (linkToConditions) LinkConditionToFlask("poisoned", flask); break;
+                        }
+                    }
+                }
+                if (quality != null)
+                {
+                    flask.LifeHealAmount = (int)((float)flask.LifeHealAmount * ((100 + quality.ItemQuality) / 100f));
+                    flask.ManaHealAmount = (int)((float)flask.ManaHealAmount * ((100 + quality.ItemQuality) / 100f));
+                    flask.Duration = (int)((float)flask.Duration * ((100 + quality.ItemQuality) / 100f));
+                }
+                return flask;
+            }
+
         }
         private static Flask[] flasks = new Flask[]
         {
-            new Flask(VirtualKeyCode.VK_1),
-            new Flask(VirtualKeyCode.VK_2),
-            new Flask(VirtualKeyCode.VK_3),
-            new Flask(VirtualKeyCode.VK_4),
-            new Flask(VirtualKeyCode.VK_5),
+            new Flask() { Key = VirtualKeyCode.VK_1 },
+            new Flask() { Key = VirtualKeyCode.VK_2 },
+            new Flask() { Key = VirtualKeyCode.VK_3 },
+            new Flask() { Key = VirtualKeyCode.VK_4 },
+            new Flask() { Key = VirtualKeyCode.VK_5 },
         };
 
         private static GameController api;
@@ -116,11 +247,39 @@ namespace Follower
         {
             if (!IsValid(api)) return;
             var playerInventories = api.Game.IngameState.ServerData.PlayerInventories;
-            if (playerInventories == null) return;
+            if (playerInventories == null || playerInventories.Count == 0)
+            {
+                if (updateFlaskMods)
+                {
+                    Log("Failed to update flasks: PlayerInventories is null or empty.");
+                }
+                updateFlaskMods = false;
+                return;
+            }
+            if (updateFlaskMods)
+            {
+                Log($"Updating {playerInventories.Count} inventories.");
+                for (int i = 0; i < playerInventories.Count; i++)
+                {
+                    var invent = playerInventories.ElementAt(i);
+                    Log($"PlayerInventory[{i}] {invent?.Address} Id:{invent?.Id} {invent?.Inventory?.InventType}");
+                }
+            }
             var flaskInventory = playerInventories.FirstOrDefault(x => x.Inventory != null && x.Inventory.InventType == InventoryTypeE.Flask);
-            if (flaskInventory == null) return;
+            if (flaskInventory == null)
+            {
+                if (updateFlaskMods) Log("Failed to update flasks: No inventory of flask type was found.");
+                updateFlaskMods = false;
+                return;
+            }
             var serverInventory = flaskInventory.Inventory;
-            if (serverInventory == null || serverInventory.InventorySlotItems == null) return;
+            if (serverInventory == null || serverInventory.InventorySlotItems == null)
+            {
+                if (updateFlaskMods) Log("Failed to update flasks: flaskInventory had no server inventory items.");
+                updateFlaskMods = false;
+                return;
+            }
+            if( updateFlaskMods ) Log("Updating flasks...");
             foreach (var item in serverInventory.InventorySlotItems)
             {
                 if (!IsValid(item)) continue;
@@ -138,16 +297,21 @@ namespace Follower
                     string pathName = flaskEnt.Path.Substring(flaskEnt.Path.LastIndexOf("/") + 1);
                     flask.PathName = pathName;
                     var quality = flaskEnt.GetComponent<Quality>();
-                    // if this is a known Life flask
-                    if (baseHealAmount.TryGetValue(pathName, out amount))
+                    if( baseDuration.TryGetValue(pathName, out flask.Duration) )
                     {
-                        flask.LifeHealAmount = amount;
+                        if( quality != null )
+                        {
+                            flask.Duration = (int)((float)flask.Duration * ((100 + quality.ItemQuality) / 100f));
+                        }
+                    } else flask.Duration = 3000;
+                    // if this is a known Life flask
+                    if (baseHealAmount.TryGetValue(pathName, out flask.LifeHealAmount))
+                    {
                         if (quality != null)
                         {
-                            flask.LifeHealAmount = (int)((float)amount * ((100 + quality.ItemQuality) / 100f));
+                            flask.LifeHealAmount = (int)((float)flask.LifeHealAmount * ((100 + quality.ItemQuality) / 100f));
                         }
-                    }
-                    else flask.LifeHealAmount = 0;
+                    } else flask.LifeHealAmount = 0;
 
                     // handle Mana flasks
                     if (baseManaAmount.TryGetValue(pathName, out amount))
@@ -181,7 +345,14 @@ namespace Follower
                                 flask.IsInstant = true;
                                 flask.LifeHealAmount = (int)((float)flask.LifeHealAmount * .25f);
                                 break;
-                            case "FlaskRemovesBleeding": LinkConditionToFlask("bleeding", flask); break;
+                            case "FlaskRemovesBleeding": 
+                                LinkConditionToFlask("bleeding", flask);
+                                LinkConditionToFlask("corrupted_blood", flask);
+                                break;
+                            case "FlaskEffectNotRemovedOnFullMana":
+                                flask.ManaHealAmount = (int)((float)flask.ManaHealAmount * .70f);
+                                flask.Duration = (int)((float)flask.Duration * .70f);
+                                break;
                             case "FlaskRemovesShock": LinkConditionToFlask("shocked", flask); break;
                             case "FlaskCurseImmunity": LinkConditionToFlask("cursed", flask); break;
                             case "FlaskDispellsChill": LinkConditionToFlask("frozen", flask); break;
@@ -220,11 +391,12 @@ namespace Follower
 
         private static bool CheckFullFlasks()
         {
+            var r = new System.Random();
             foreach(var flask in flasks)
             {
-                if( flask.IsFull && flask.LifeHealAmount == 0 && flask.ManaHealAmount == 0 && flask.IsUsable(2000) )
+                if( flask.IsFull && flask.LifeHealAmount == 0 && flask.ManaHealAmount == 0 && flask.UseFlask(3000 + r.Next(1,100)) )
                 {
-                    return flask.UseFlask(2000);
+                    return true;
                 }
             }
             return false;
@@ -275,7 +447,7 @@ namespace Follower
             var maxHp = life.MaxHP - life.ReservedFlatHP;
             var curHp = life.CurHP;
             var lifeThreshold = maxHp / 2;
-            DrawTextAtPlayer($"CheckLifeFlasks: {curHp} > {lifeThreshold}");
+            // DrawTextAtPlayer($"CheckLifeFlasks: {curHp} > {lifeThreshold}");
             if (curHp <= lifeThreshold)
             {
                 Flask instantFlask = null;
@@ -283,7 +455,7 @@ namespace Follower
                 foreach (var flask in flasks)
                 {
                     if (!(flask.LifeHealAmount > 0 && flask.IsUsable(200))) continue;
-                    if (flask.IsInstant || (flask.IsInstantOnLowLife && life.HPPercentage < .5))
+                    if (flask.IsInstant || (flask.IsInstantOnLowLife && life.HPPercentage <= .5))
                     {
                         instantFlask = instantFlask ?? flask;
                     }
